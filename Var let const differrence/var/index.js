fac0e2ecf7d68039de4e4903930e006a2c6fe1b4
sawwalakhepallavi <101243878+sawwalakhepallavi@users.noreply.github.com>
2023-02-20 23:34:06 +0530

    Add files via upload

---
+++ Var let const differrence/var/index.js
@@ -0,0 +1,27 @@
+//declaration
+var a;
+console.log(a);
+
+//initialization
+var b;
+b=10;
+console.log(b);
+
+//declaration & inialization
+var c="hello"
+console.log(c);
+
+//redeclaration
+var d;
+d="good night"
+d="good morning"
+console.log(d);
+
+//redeclaration & inialization
+var e="Akshay"
+e="Akshay kumar"
+console.log(e);
+
+//hoisting
+console.log(f);
+var f=10;
