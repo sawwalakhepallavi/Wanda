0b1a826b9f3c50a68160f6d153efcd6c98ac77c7
sawwalakhepallavi <101243878+sawwalakhepallavi@users.noreply.github.com>
2023-03-01 15:26:45 +0530

    Add files via upload

---
+++ javascript/Math/Math.js
@@ -0,0 +1,21 @@
+// random create a random value....0.1 to 0.9
+let a= Math.random()     
+console.log(a);
+
+// its take the round figu
+let b=Math.round(4.4)
+console.log(b);
+
+// its consist the height value
+let c=Math.ceil(4.3)
+console.log(c);
+
+// its consist of lowest value
+let d=Math.floor(4.9)
+console.log(d);
+
+let e=Math.sqrt(25)
+console.log(e);
+
+let f=Math.pow(4,2)
+console.log(f);
