258d1b8589311b9e93735307f9e131acc51d0380
sawwalakhepallavi <101243878+sawwalakhepallavi@users.noreply.github.com>
2023-03-02 18:07:17 +0530

    Add files via upload

---
+++ javascript/Conditions/cond.js
@@ -0,0 +1,45 @@
+let a=2
+if(a%2==0){
+    console.log("even");
+}
+
+let b=35
+if (b%2==0) {
+    console.log("even");
+} else {
+    console.log("odd");
+}
+
+// eles if condition
+let age=21;
+if (age==5) {
+    console.log("join kg1");
+}
+else if(age==6){
+    console.log("join kg2");
+}
+else if(age==7){
+    console.log("join 1st");
+}
+else{
+    console.log("please enter correct age");
+}
+
+
+// switch case
+let time=8;
+switch (time) {
+    case 6:
+        console.log("good morning");
+        break;
+    case 12:
+        console.log("good afternoon");
+        break;
+    case 18:
+        console.log("good evening");
+        break;
+
+    default:
+        console.log("enter correct time");
+        break;
+}
