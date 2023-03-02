a8bfa96bbdf977e78ae45651be77fd064f6d7bf3
sawwalakhepallavi <101243878+sawwalakhepallavi@users.noreply.github.com>
2023-03-02 18:08:39 +0530

    Add files via upload

---
+++ javascript/nonprimitive function/Date/date.js
@@ -0,0 +1,20 @@
+let a=new Date()
+console.log(a);
+
+let b=a.getDay()
+console.log(b);
+
+let c=a.getMonth()
+console.log(c);
+
+let d=a.getFullYear()
+console.log(d);
+
+let e=a.getHours()
+console.log(e);
+
+let f=a.getMilliseconds()
+console.log(f);
+
+let g=a.getSeconds()
+console.log(g);
