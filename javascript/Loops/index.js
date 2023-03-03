2ece75731a37c605129329b2ba9cd193f6c8edb3
sawwalakhepallavi <101243878+sawwalakhepallavi@users.noreply.github.com>
2023-03-03 15:21:11 +0530

    Add files via upload

---
+++ javascript/Loops/index.js
@@ -0,0 +1,17 @@
+let i=0
+let n=5
+while(i<=n){
+    console.log(i);
+    i++;
+}
+
+
+// do{
+//     console.log(i);
+//     i++;
+// }while (i<=5);
+
+    
+for(let i=2;i<=5;i++){
+    console.log(i);
+}
