f2fdb64716e19016b41edb7a0a672cfb01e492f6
sawwalakhepallavi <101243878+sawwalakhepallavi@users.noreply.github.com>
2023-03-09 23:41:13 +0530

    Add files via upload

---
+++ Meta Practice/registration from/index.js
@@ -0,0 +1,13 @@
+function fetch(){
+    let name=document.getElementById('name').value
+    let email=document.getElementById('email').value
+    let password=document.getElementById('password').value
+    let gender=document.getElementById('Gender').value
+    let branch=document.getElementById('Branch').value
+    document.writeln('Name:'+name)
+    document.writeln('email:'+email)
+    document.writeln('password:'+password)
+    document.writeln('gender:'+gender)
+    document.writeln('branch:'+branch)
+
+}
